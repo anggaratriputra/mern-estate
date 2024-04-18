c0544632592ccf6ab9179ecc75d388ef0af10491
anggaratriputra <anggaratriputra@gmail.com>
2024-04-18 15:19:52 +0700

    setting up redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
- 
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -18,7 +19,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,21 +28,17 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success === false) {
-        setError(data.message);
-        setLoading(false);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false)
-      setError(null)
-      navigate("/")
-      
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message)
+      dispatch(signInFailure(error.message));
     }
   };
-  console.log(formData)
+  console.log(formData);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -50,7 +47,8 @@ export default function SignIn() {
         <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
         <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? 'Signing In...' : 'Sign In' }</button>
+          {loading ? "Signing In..." : "Sign In"}
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
