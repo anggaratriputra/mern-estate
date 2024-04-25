a52eda9e7f8586222a12498cf1adde38d9f160cc
anggaratriputra <anggaratriputra@gmail.com>
2024-04-25 11:04:12 +0700

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -26,12 +28,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
