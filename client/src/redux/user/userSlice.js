2a76a2ce04977e09d0ca5948f01945586230a381
anggaratriputra <anggaratriputra@gmail.com>
2024-04-25 13:18:39 +0700

    add delete account functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,11 +33,24 @@ const userSlice = createSlice ({
         updateUserFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        }
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+
 
     }   
 })
 
-export const { signInStart, signInFailure, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess } = userSlice.actions;
+export const { signInStart, signInFailure, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess, deleteUserStart, deleteUserFailure, deleteUserSuccess } = userSlice.actions;
 
 export default userSlice.reducer;
